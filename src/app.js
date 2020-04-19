9d9a16b347b0d37183cd9e090d1251b47d3fae17
Juan Camilo Velásquez Vanegas <juancamilo---142@hotmail.com>
2020-04-19 16:33:28 -0500

    Api route not found handler.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { NOT_FOUND } from 'http-status'
 import listEndpoints from 'express-list-endpoints'
 import api from 'routes'
 import { OK } from 'http-status'
 import 'dotenv/config'
 import { errorsHandler } from 'middlewares/errors.middleware'
+import { ApiException } from 'utils/errors.exceptions'
 
 const prefix = process.env.PREFIX || '/api'
 const app = express()
@@ -16,6 +18,7 @@ app.use(cors())
 
 app.get(prefix, (req, res) => res.status(OK).send({ routes: listEndpoints(app) }))
 app.use(prefix, api)
+app.use((req, res, next) => next(new ApiException('Api route not found', NOT_FOUND)))
 app.use(errorsHandler)
 
 export default app
